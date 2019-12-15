9f835e609f2e6e59b9344940713f4df74a5c419a
Wendy Garcia <wendy.fern84@gmail.com>
2019-12-15 13:31:33 -0800

    created and added a search bar component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
   return (
     <div className="App">
+      <SearchBar />
       <BusinessList />
     </div>
   );
