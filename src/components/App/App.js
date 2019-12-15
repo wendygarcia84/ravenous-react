42051f6cd9d11e969be54e382e9a6f678bed07f9
Wendy Garcia <wendy.fern84@gmail.com>
2019-12-15 11:22:14 -0800

    created business list app and rendered businesses in this component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from '../Header';
-import Business from '../Business/Business';
+import BusinessList from '../BusinessList/BusinessList';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Business />
+      <BusinessList />
     </div>
   );
 }
