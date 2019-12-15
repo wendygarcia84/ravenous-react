b73d58800f49cfcaa63eb7132054cb26803f64dd
Wendy Garcia <wendy.fern84@gmail.com>
2019-12-14 18:25:41 -0800

    Updates

---
+++ precache-manifest.5d36a23dc2e20fba53ba99409fa7cb8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de7d546e831d396e1db5bceab1669b17",
+    "url": "/ravenous-react/index.html"
+  },
+  {
+    "revision": "b72525f837c8b81aeb66",
+    "url": "/ravenous-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d7e88721189b3c6adc54",
+    "url": "/ravenous-react/static/js/2.c90d963f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ravenous-react/static/js/2.c90d963f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b72525f837c8b81aeb66",
+    "url": "/ravenous-react/static/js/main.b74418b6.chunk.js"
+  },
+  {
+    "revision": "603651b591eddec0b87a",
+    "url": "/ravenous-react/static/js/runtime-main.93f144f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ravenous-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
