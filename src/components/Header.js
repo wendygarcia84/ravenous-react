ada47cf8f2e387bd558b9d9e45066b268a7f992b
Wendy Garcia <wendy.fern84@gmail.com>
2019-12-15 10:27:51 -0800

    Header component function and file created, header component instantiated in App component class

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Header(){
+    return (
+    <header className="App-header">
+        <p>
+          Ravenous project in progres...
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      );
+}
+
+export default Header;
