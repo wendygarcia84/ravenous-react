f8cd800689ea244392886f44e030cd9b8bf3d9e4
Wendy Garcia <wendy.fern84@gmail.com>
2020-01-08 09:45:57 -0800

    menu changes classes with click, and selected item changes color

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -18,10 +18,22 @@ class SearchBar extends React.Component {
         };
     }
 
+    getSortByClass(sortByOption) {
+        if (this.state.sortBy === sortByOption) {
+            return "active";
+        } else {
+            return "";
+        }
+    }
+
+    handleSortByChange(sortByOption) {
+        this.setState({sortBy: sortByOption});
+    }
+
     renderSortByOptions() {
         return Object.keys(this.sortByOptions).map(sortByOption => {
             let sortByOptionValue = this.sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>
+            return <li key={sortByOptionValue} className={this.getSortByClass(sortByOptionValue)} onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>{sortByOption}</li>
         });
     }
 
