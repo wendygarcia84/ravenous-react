f728e260d3891ade2b42ce266c8a20c03f1b1912
Wendy Garcia <wendy.fern84@gmail.com>
2020-01-16 14:34:16 -0800

    create Yelp object and search function and return fetch method

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,2 +1,6 @@
 const apiKey = "QzKq3Wne0xKUz1AHPjk4hFda3hNeZ210GVfA07zOvnOkBNe6MIYrJqN51tK5Prn3e8w8EcgJurCkPGEDRIFELha91uUd55KY9fyWGbpTfYEKx3CxLj4OHEZH3pL2XXYx";
+let Yelp = {};
 
+function search(term, location, sortBy) {
+    return fetch();
+}
