29117d8f7d90d44b38bacafaa816fbf5f30044bc
Wendy Garcia <wendy.fern84@gmail.com>
2020-01-16 15:06:24 -0800

    call to yelp api and returning an object with the required data

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,6 +1,31 @@
+import SearchBar from "../components/SearchBar/SearchBar";
+
 const apiKey = "QzKq3Wne0xKUz1AHPjk4hFda3hNeZ210GVfA07zOvnOkBNe6MIYrJqN51tK5Prn3e8w8EcgJurCkPGEDRIFELha91uUd55KY9fyWGbpTfYEKx3CxLj4OHEZH3pL2XXYx";
 let Yelp = {};
 
-function search(term, location, sortBy) {
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {headers={authorization=`Bearer ${apiKey}`}});
+function Search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {headers:{authorization:`Bearer ${apiKey}`}})
+    .then(response => {response.json()})
+    .then(jsonResponse => {
+        if(jsonResponse.businesses) {
+            console.log(jsonResponse);
+            return response.businesses.map(business => {
+                return {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.category[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                }
+            });
+        }
+    });
+    
 }
+
+export default Search;
